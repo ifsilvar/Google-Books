e9b5d5c51f61ca8b33293fd961abd399c996f804
Ivan Flores <ifsilvar4@gmail.com>
2021-02-21 21:25:02 -0800

    added code for api and input change

---
+++ models/books.js
@@ -0,0 +1,14 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const bookSchema = new Schema({
+//   authors: [ String ],
+//   description: { type: String, required: true },
+//   image: String,
+//   link: String,
+//   title: String
+// });
+
+// const Book = mongoose.model("Book", bookSchema);
+
+// module.exports = Book;
